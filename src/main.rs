181cf7f034ed6efabb2e72ec1b79a327aba1ad5c
Barcode-Snow <51758168+Barcode-Snow@users.noreply.github.com>
2023-10-09 13:35:31 +0200

    I hate AI

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use std::thread;
-
-fn main() {
-    thread::Builder::new().stack_size(32*1024*1024).spawn(move || {
-        for i  in 1..=100 {
-            let m: i128 = i;
-            let n: i128 = i;
-            let result = ackermann(m, n);
-            println!("Ackermann({}, {}) = {}", m, n, result);
+fn ackermann(m: i128, n: i128) -> i128 {
+    let mut stack = Vec::new();
+    let mut m = m;
+    let mut n = n;
+    
+    loop {
+        if m == 0 {
+            if let Some((prev_m, n)) = stack.pop() {
+                m = prev_m - 1;
+                stack.push((m, n + 1));
+            } else {
+                return n + 1;
+            }
+        } else if n == 0 {
+            m -= 1;
+            n = 1;
+        } else {
+            stack.push((m - 1, n));
+            n -= 1;
         }
-    }).unwrap();
+    }
 }
 
-fn ackermann(m: i128, n: i128) -> i128 {
-    if m == 0 {
-        n + 1
-    } else if n == 0 {
-        ackermann(m - 1, 1)
-    } else {
-        ackermann(m - 1, ackermann(m, n - 1))
+fn main() {
+    for i in 1..=100 {
+        for j in 1..=100 {
+            let result = ackermann(i, j);
+            println!("Ackermann({}, {}) = {}", i, j, result);
+        }
     }
 }
